deeda2f667e834e460ac2630e547b0e88ad3cae5
Sam Allen <samaallen@gmail.com>
2022-12-02 16:05:13 +0000

    Add project files.

---
+++ Day1/Calories.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public static class Calories
+    {
+        public static List<Elf> ConvertData(List<string> data)
+        {
+            List<Elf> elves = new List<Elf>();
+            int elvesCount = 0;
+            int currentCalories = 0;
+            int currentIndex = 0;
+            foreach (var calorie in data)
+            {
+                bool isCalorie = int.TryParse(calorie, out int currentCalorie);
+                if (isCalorie)
+                {
+                    currentCalories += currentCalorie;
+                }
+                else
+                {
+                    elvesCount++;
+                    elves.Add(new Elf()
+                    {
+                        Number = elvesCount,
+                        TotalCalories = currentCalories
+                    });
+
+                    if (currentIndex != data.Count)
+                        currentCalories = 0;
+                }
+                
+                currentIndex++;
+            }
+            elves.Add(new Elf()
+            {
+                Number = elvesCount,
+                TotalCalories = currentCalories
+            });
+
+            return elves;
+        }
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventHelper;
+using Day1;
+
+var allLines = AdventTextReader.GetListFromFile("data.txt", true);
+Console.WriteLine(allLines.Count);
+var elves = Calories.ConvertData(allLines);
+var maxCalories = elves.Max(x => x.TotalCalories);
+Console.WriteLine(maxCalories); 
+Console.ReadLine();
