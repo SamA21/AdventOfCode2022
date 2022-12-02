9c849d4cb83afb90e107bef5111adcef1e7e4adf
Sam Allen <samaallen@gmail.com>
2022-12-02 16:08:18 +0000

    Day 1 done

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -6,4 +6,6 @@ Console.WriteLine(allLines.Count);
 var elves = Calories.ConvertData(allLines);
 var maxCalories = elves.Max(x => x.TotalCalories);
 Console.WriteLine(maxCalories); 
+var maxCalories3Elves = elves.OrderByDescending(x => x.TotalCalories).Take(3).Sum(x => x.TotalCalories);
+Console.WriteLine(maxCalories3Elves);
 Console.ReadLine();
