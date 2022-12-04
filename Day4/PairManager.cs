7275467c368bc3d96a2cd75c60cab4d4bf0214ad
Sam Allen <samaallen@gmail.com>
2022-12-04 13:50:32 +0000

    Day 4 changed fixed values to range

--- Day4/PairManager.cs
+++ Day4/PairManager.cs
@@ -8,10 +8,8 @@ namespace Day4
 {
     public sealed class PairManager
     {
-        public int Pair1Min { get; init; }
-        public int Pair1Max { get; init; }
-        public int Pair2Min { get; init; }
-        public int Pair2Max { get; init; }
+        public IEnumerable<int> Pair1Range { get; init; } = new List<int>();
+        public IEnumerable<int> Pair2Range { get; init; } = new List<int>();
 
         public PairManager(string pairLine)
         {
@@ -20,45 +18,37 @@ namespace Day4
             {
                 if (pairs.First().Equals(pair))
                 {
-                    var numbers = GetNumbers(pair);
-                    Pair1Min = numbers.Item1;
-                    Pair1Max = numbers.Item2;
-
+                    Pair1Range = GetNumbers(pair); 
                 }
 
                 if (pairs.Last().Equals(pair))
                 {
-                    var numbers = GetNumbers(pair);
-                    Pair2Min = numbers.Item1;
-                    Pair2Max = numbers.Item2;
+                    Pair2Range = GetNumbers(pair); 
                 }
             } 
         }
 
         public bool CheckIfFullyContain()
         {
-            var pair1 = Enumerable.Range(Pair1Min, Pair1Max - Pair1Min + 1);
-            var pair2 = Enumerable.Range(Pair2Min, Pair2Max - Pair2Min + 1);
-            if (pair1.Intersect(pair2).Count() == pair1.Count() || pair2.Intersect(pair1).Count() == pair2.Count())
+            if (Pair1Range.Intersect(Pair2Range).Count() == Pair1Range.Count() || Pair2Range.Intersect(Pair1Range).Count() == Pair2Range.Count())
                 return true;
             else
                 return false;
         }
         public bool CheckIfRangesOverlap()
         {
-            var pair1 = Enumerable.Range(Pair1Min, Pair1Max - Pair1Min + 1);
-            var pair2 = Enumerable.Range(Pair2Min, Pair2Max - Pair2Min + 1);
-            if (pair1.Where(x => pair2.Contains(x)).Count() > 0 || pair2.Where(x => pair1.Contains(x)).Count() > 0)
+            if (Pair1Range.Where(x => Pair2Range.Contains(x)).Count() > 0 || Pair2Range.Where(x => Pair1Range.Contains(x)).Count() > 0)
                 return true;
             else
                 return false;
         }
-        private (int, int) GetNumbers(string pair)
+        private IEnumerable<int> GetNumbers(string pair)
         {
             var numbers = pair.Split('-');
             int number1 = int.Parse(numbers[0]);
             int number2 = int.Parse(numbers[1]);
-            return (number1, number2);
+
+            return Enumerable.Range(number1, number2 - number1 + 1);
         }
     }
 }
